c9191ac167f197816c8692e9c76d05dd45c5bb07
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2024-02-24 12:07:09 +0200

    Update Ex3.cs

--- Lab2/Ex3.cs
+++ Lab2/Ex3.cs
@@ -1,1 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Ex3a
+{
+    class Program
+    {
+        //functia principala, parcurge lungimile posibile si apeleaza functia recursiva
+        static List<string> GenerateStrings(char[] characters, int maxlen)
+        {
+            List<string> stringslist = new List<string>();
+            for (int length = 1; length <= maxlen; length++)
+            {
+                GenerateStringRec(characters, length, 0, "",stringslist);
+            }
+            return stringslist;
+        }
+        //functie recursiva care genereaza toate combinatiile posibile pentru caracterele din characters care sa fie de lungimea data de maxlen
+        static void GenerateStringRec(char[] characters, int maxlen, int currentlen, string current,List<string> stringslist)
+        {
+            if (currentlen == maxlen)
+            {
+                stringslist.Add(current);
+                return;
+            }
+
+            foreach (char character in characters)
+            {
+                GenerateStringRec(characters, maxlen, currentlen + 1, current + character,stringslist);
+            }
+        }
+        static void Main()
+        {
+            char[] characters = { 'a', 'b' };
+            int maxlen = 5;
+            //enuntul nu era clar pentru ca zicea doar "de lungime maxima", fara un numar care sa fie lungimea maxima,
+            //asa ca am luat 3 ca lungime maxima, dar poate fi schimbata valoarea la maxlen pentru alta lungime maxima
+            List<string> stringslist=GenerateStrings(characters, maxlen);
+            foreach(string s in stringslist)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
 
