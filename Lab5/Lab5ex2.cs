f2ea14b6e52d850c1bb7cbb5bab775817fdf9b5b
LucaciGabriel <114673169+LucaciGabriel@users.noreply.github.com>
2024-03-14 19:22:30 +0200

    Create Lab5ex2.cs

---
+++ Lab5/Lab5ex2.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepgoing = true;
+            int currentstate = 1;
+            char input = 'A';
+            string result="O1";
+            while (keepgoing)
+            {
+                switch (currentstate)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Sunteti in starea 1, tastati 'A' pentru starea 1, 'B' pentru starea 2, 'X' pentru a opri aplicatia");
+                            input=char.Parse(Console.ReadLine());
+                            if(input =='A')
+                            {
+                                currentstate = 1;
+                                result=result+"O1";
+                            }
+                            else if(input == 'B')
+                                {
+                                    currentstate = 2;
+                                    result = result + "02";
+                                }
+                                else if(input == 'X')
+                                    {
+                                        keepgoing= false;
+                                    }
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Sunteti in starea 2, tastati 'A' pentru starea 1, 'B' pentru starea 2, 'X' pentru a opri aplicatia");
+                            input = char.Parse(Console.ReadLine());
+                            if (input == 'A')
+                            {
+                                currentstate = 1;
+                                result = result + "O1";
+                            }
+                            else if (input == 'B')
+                                {
+                                    currentstate = 2;
+                                    result = result + "02";
+                                }
+                                else if (input == 'X')
+                                    {
+                                        keepgoing = false;
+                                    }
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine("Rezulatul final este " + result);
+        }
+    }
+}
